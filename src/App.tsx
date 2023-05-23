85c5d907a30dc02242908b3b4551476853aa984a
Slavik <theslavprod@gmail.com>
2023-05-24 00:20:56 +0400

    card app first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import { ReactComponent as RemoveIcon } from "./assets/icons8-close.svg";
+import { AppContainer } from "./app.styled";
 
 function App() {
+  const savedCards = localStorage.getItem("cards");
+  const initialCards = savedCards ? JSON.parse(savedCards) : [];
+
+  const [cards, setCards] = useState<number[]>(initialCards);
+
+  useEffect(() => {
+    localStorage.setItem("cards", JSON.stringify(cards));
+  }, [cards]);
+
+  const generateRandomUniqueNumber = (): number => {
+    let randomNum = Math.floor(Math.random() * 100);
+    while (cards.includes(randomNum)) {
+      randomNum = Math.floor(Math.random() * 100);
+    }
+    return randomNum;
+  };
+
+  const handleAddCard = () => {
+    const randomNum = generateRandomUniqueNumber();
+    setCards([...cards, randomNum]);
+  };
+
+  const handleSortCards = () => {
+    const sortedCards = [...cards].sort((a, b) => a - b);
+    setCards(sortedCards);
+  };
+
+  const handleRemoveCard = (index: number) => {
+    const updatedCards = [...cards];
+    updatedCards.splice(index, 1);
+    setCards(updatedCards);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer className="App">
+      <div className="mainContent">
+        <header className="header">
+          <button className="btn" onClick={handleAddCard}>Add card</button>
+          <button className="btn" onClick={handleSortCards}>Sort cards</button>
+        </header>
+        <main className="main">
+          {cards.map((card, index) => (
+            <div key={index} className="card">
+              {card}
+              <RemoveIcon
+                className="removeIcon"
+                onClick={() => handleRemoveCard(index)}
+              />
+            </div>
+          ))}
+        </main>
+        <footer className="footer">Footer</footer>
+      </div>
+      <div className="instructionBlock">
+        <aside className="aside">
+          <p className="instructionText">
+          Press the "Add card" button to add the new Card. Use the "sort cards"
+          button to sort the Cards by the increase. Press an X icon on the top
+          right to delete them.
+          </p>
+        </aside>
+      </div>
+    </AppContainer>
   );
 }
 
